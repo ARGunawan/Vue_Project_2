be49f72ba5e09a83feed722ca6cdba6fcfc630d3
Alfonso Gunawan <44307331+ARGunawan@users.noreply.github.com>
2023-01-10 13:25:49 -0500

    added event listeners

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "",
     };
   },
   methods:{
-    increment(){
-      this.counter++
+    increment(num){
+      this.counter = this.counter + num
     },
-    decrement(){
-      this.counter--
+    decrement(num){
+      this.counter = this.counter - num
+    },
+    setName(event, lastName){
+      this.name = event.target.value + ' ' + lastName;
     }
   }
 });
